b00e83000a749dd64eee2b5f32ead814e28dec21
Vadym Bondarenko <danxil@list.ru>
2019-02-26 17:45:36 +0200

    Adding how to page

--- routes/handlers/tariffs.js
+++ routes/handlers/tariffs.js
@@ -1,6 +1,6 @@
 import { getTariffs } from '../../controllers/tariffs';
 
 export default () => async (req, res) => {
-  const tariffs = await getTariffs({ withTests: req.user.isAdmin });
+  const tariffs = await getTariffs({ withTests: req.user && req.user.isAdmin });
   return res.send(tariffs);
 };

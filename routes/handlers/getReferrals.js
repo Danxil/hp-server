d6351e4fa773f813adc7c5746eeccfbaca5cc675
Vadym Bondarenko <danxil@list.ru>
2019-03-02 00:23:05 +0200

    Changed ci

--- routes/handlers/getReferrals.js
+++ routes/handlers/getReferrals.js
@@ -4,7 +4,7 @@ import { getReferrals } from '../../controllers/user';
 export default () => async ({ user: { id: userId } }, res) => {
   const referrals = await getReferrals({ userId });
   const investments = await getAll({ userId });
-  const totalInvested = investments.reduce((prev, { amount }) => prev + amount);
+  const totalInvested = investments.reduce((prev, { amount }) => prev + amount, 0);
   const result = referrals.map(referral => ({ ...referral.toJSON(), totalInvested }));
   return res.send(result);
 };

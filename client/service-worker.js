66acb5c8d4963f736e5e8d741084e8e24c278fd8
Vadym Bondarenko <danxil@list.ru>
2019-04-02 19:46:40 +0300

    changed cli

--- client/service-worker.js
+++ client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054d32998f0867d7bacfff60297bbe57.js"
+  "/precache-manifest.15ff07214db5806e244e674f98024987.js"
 );
 
 workbox.clientsClaim();
